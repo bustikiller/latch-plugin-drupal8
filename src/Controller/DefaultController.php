2daae56b334c99bc323c48b477a9fa005505566e
Manuel Bustillo <mayn13@gmail.com>
2022-08-13 16:44:34 +0200

    Remove debugging message

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -172,7 +172,6 @@ class DefaultController extends ControllerBase {
 	}
 
 	public function pairingFormAccess(AccountInterface $account, $user = NULL) {
-		\Drupal::messenger()->addStatus('Checking if user '.$account->id().' has access to pair account of user '.$user);
 		return AccessResult::allowedIf($account->id() == $user);
 	}
 }
